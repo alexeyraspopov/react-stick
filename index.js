8872a97635e8263bec806b1bd22c3aa93bb9fded
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-09-21 10:20:52 +0300

    CSS styles for native support

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React from "react";
 
 export default positionStickyIsSupported() ? NativeSticky : null;
 
-const NATIVE_STICKY_DEFINITION = 'position: -webkit-sticky; position: -moz-sticky; position: sticky;';
+const NATIVE_STICKY_CLASSNAME = "react-native-sticky-element-styles";
+const NATIVE_STICKY_DEFINITION = `
+	.${NATIVE_STICKY_CLASSNAME} {
+		position: -webkit-sticky;
+		position: -moz-sticky;
+		position: sticky;
+	}
+`;
 
 class NativeSticky extends React.Component {
 	getDefaultProps() {
 		return {
-			type: 'div',
+			type: "div",
 		};
 	}
 
 	render() {
-		// HACK: capitalized name is used because Babel will pass it's value to React.createElement.
+		/**
+		 * HACK: capitalized name is used because Babel will pass it's value to React.createElement.
+		 * Example:
+		 *   <boo /> => React.createElement("boo", ...)
+		 *   var Boo = "boo2"
+		 *   <Boo /> => React.createElement(Boo, ...)
+		 */
 		var Root = this.props.type;
 
 		return (
-			<Root style={NATIVE_STICKY_DEFINITION}>{this.props.children}</Root>
+			<Root className={NATIVE_STICKY_CLASSNAME}>
+				<style>{NATIVE_STICKY_DEFINITION}</style>
+				{this.props.children}
+			</Root>
 		);
 	}
 }
